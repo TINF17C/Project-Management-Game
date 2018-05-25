ce9fc13eaebcebea145a6fe38ec431d9c80269a4
Constantin Ruhdorfer <constantin.ruhdorfer@gmx.de>
2018-05-25 10:41:29 +0200

    Building an example nav bar without links

--- PMGame/src/app/app.module.ts
+++ PMGame/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HeaderComponent } from './header/header.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
+    HeaderComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
--- PMGame/src/app/material.module.ts
+++ PMGame/src/app/material.module.ts
@@ -7,34 +7,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // Declares the Material Modules we want to use
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatListModule } from '@angular/material/list';
-import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @NgModule({
     imports: [
-        MatButtonModule,
-        MatCardModule,
         MatToolbarModule,
-        MatIconModule,
-        MatTableModule,
-        MatListModule,
-        MatProgressBarModule
+        MatButtonModule
     ],
     exports: [
-        MatButtonModule,
-        MatCardModule,
         MatToolbarModule,
-        MatIconModule,
-        MatTableModule,
-        MatListModule,
-        MatProgressBarModule
+        MatButtonModule
     ]
 })
 export class MaterialModule {}
