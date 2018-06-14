e8d5ea7037c3041520908ed8d294dbc59cac7ef3
VuQuangPhuc <phuc.vu-quang@gmx.de>
2018-06-14 23:03:39 +0200

    home screen

--- PMGame/src/app/app-routing.module.ts
+++ PMGame/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './views/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'game',
+    component: HomeComponent
+  },
+  {
+    path: 'rules',
+    component: HomeComponent
+  },
+  {
+    path: 'impressum',
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- PMGame/src/app/app.module.ts
+++ PMGame/src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { HeaderComponent } from './header/header.component';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MaterialModule } from './material.module';
+import { HomeMenuComponent } from './components/home-menu/home-menu.component';
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MaterialModule } from './material.module';
+import { HomeComponent } from './views/home/home.component';
 
 @NgModule({
-  declarations: [HeaderComponent, AppComponent, FooterComponent],
+  declarations: [
+    HeaderComponent,
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    HomeMenuComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
