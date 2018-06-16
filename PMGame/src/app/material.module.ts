9c41f87543cbda1109e66cbf9bb25e77733051a9
VuQuangPhuc <phuc.vu-quang@gmx.de>
2018-06-16 14:42:44 +0200

    added game components

--- PMGame/src/app/material.module.ts
+++ PMGame/src/app/material.module.ts
@@ -9,17 +9,24 @@ import { CommonModule } from '@angular/common';
 // Declares the Material Modules we want to use
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-
-
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatRadioModule } from '@angular/material/radio';
 
 @NgModule({
-    imports: [
-        MatToolbarModule,
-        MatButtonModule
-    ],
-    exports: [
-        MatToolbarModule,
-        MatButtonModule
-    ]
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatRadioModule
+  ],
+  exports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatRadioModule
+  ]
 })
 export class MaterialModule {}
