7348422728b50e22387183c74d6b61d00b6db8c2
pa6ne6 <pa6ne6@PC474W>
2018-06-25 09:09:55 +0200

    Rearranged code

--- PMGame/src/app/views/game/game.component.ts
+++ PMGame/src/app/views/game/game.component.ts
@@ -22,6 +22,7 @@ export class GameComponent implements OnInit, AfterViewInit {
   // player objects
   players: IPlayer[];
   currentPlayer: number;
+  isGameOver : boolean;
 
   // progress bar view
   color = 'primary';
@@ -41,12 +42,11 @@ export class GameComponent implements OnInit, AfterViewInit {
 
   startDialogRef: MatDialogRef<StartDialogComponent>;
   winnerDialogRef: MatDialogRef<WinnerDialogComponent>;
-  winnerString = '';
 
   constructor(
     public controller: GameControllerService,
     public dialog: MatDialog
-  ) {}
+  ) { }
 
   ngOnInit() {}
 
@@ -60,6 +60,7 @@ export class GameComponent implements OnInit, AfterViewInit {
       this.startDialogRef.afterClosed().subscribe((r) => {
         this.players = this.controller.initNewPlayers(r);
         this.currentPlayer = 0;
+        this.isGameOver = false;
       });
     });
   }
@@ -68,11 +69,23 @@ export class GameComponent implements OnInit, AfterViewInit {
    * When you click the bribe button this function is executed. It reduces money, keeps the rank and ends the turn.
    */
   bribe() {
-    // this.controller.changePlayerTitle(this.players[this.currentPlayer], 0);
-    this.controller.changePlayerMoney(
-      this.players[this.currentPlayer],
-      this.question.difficulty * -300
-    );
+
+    //  TODO: Check if any answer is selected.
+
+    if(this.isGameOver) {
+      // TODO: Show Game Over dialog. Maybe start a new game?
+      alert("Game is already over!");
+      return;
+    }
+
+    if(!this.isAbleToBribe()) {
+      //  TODO: Show some dialog.
+      alert("Cannot bribe! Not enough money.");
+      return;
+    }
+
+
+    this.punishCurrentPlayer(300, false);
     this.nextTurn();
   }
 
@@ -81,24 +94,22 @@ export class GameComponent implements OnInit, AfterViewInit {
    * and title and ends the turn.
    */
   submit() {
-    if (
-      this.question.options[this.question.correctAnswer - 1] ===
-      this.selectedAnswer
-    ) {
-      // console.log("Correct!");
-      this.controller.changePlayerTitle(this.players[this.currentPlayer], 1);
-      this.controller.changePlayerMoney(
-        this.players[this.currentPlayer],
-        this.question.difficulty * 100
-      );
-    } else {
-      // console.log("Wrong!");
-      this.controller.changePlayerTitle(this.players[this.currentPlayer], -1);
-      this.controller.changePlayerMoney(
-        this.players[this.currentPlayer],
-        this.question.difficulty * -100
-      );
+
+    //  TODO: Check if any answer is selected.
+
+    if(this.isGameOver) {
+      // TODO: Show Game Over dialog. Maybe start a new game?
+      alert("Game is already over!");
+      return;
     }
+
+    if (this.isCorrectAnswer()) {
+      this.rewardCurrentPlayer(100);
+    } else {
+      //  TODO: Show punishment dialog.
+      this.punishCurrentPlayer(100, true);
+    }
+
     this.nextTurn();
   }
 
@@ -111,11 +122,61 @@ export class GameComponent implements OnInit, AfterViewInit {
     if (this.currentPlayer >= this.players.length) {
       this.currentPlayer = 0;
       this.controller.incrementStageCount(this.game);
-      if (this.game.stage === 9) {
+      if (this.game.stage === this.stages.length) {
         this.gameEnds();
       }
     }
+
     this.fetchNewQuestion();
+  }
+
+  /**
+   * Returns the current player.
+   */
+  getCurrentPlayer() : IPlayer {
+    return this.players[this.currentPlayer];
+  }
+
+  /**
+   * Checks if the correct answer is selected.
+   * Note: Does not check whether no answer is selected!
+   */
+  isCorrectAnswer() : boolean {
+    return this.question.options[this.question.correctAnswer - 1] === this.selectedAnswer;
+  }
+
+  /**
+   * Checks if the player has enough money to bribe.
+   */
+  isAbleToBribe() : boolean {
+    return this.getCurrentPlayer().money >= this.question.difficulty * 300;
+  }
+
+  /**
+   * Rewards the current player according to the questions difficulty.
+   * @param factor Multiplication factor for the questions difficulty.
+   */
+  rewardCurrentPlayer(factor: number) {
+    let player = this.getCurrentPlayer();
+    let reward = this.question.difficulty * factor;
+
+    this.controller.changePlayerTitle(player, 1);
+    this.controller.changePlayerMoney(player, reward);
+  }
+
+  /**
+   * Punishes the current player.
+   * @param factor The multiplication factor for the questions difficulty. It is negated automatically.
+   * @param changeTitle Determines whether the title of the player should be reduced too.
+   */
+  punishCurrentPlayer(factor: number, changeTitle: boolean) {
+    let player = this.getCurrentPlayer();
+    let punishment = this.question.difficulty * -factor;
+
+    if(changeTitle)
+      this.controller.changePlayerTitle(player, -1);
+
+    this.controller.changePlayerMoney(player, punishment);
   }
 
   /**
@@ -129,6 +190,11 @@ export class GameComponent implements OnInit, AfterViewInit {
    * This function checks who the winner is, hands that information to the winner-dialog and displays that.
    */
   gameEnds() {
+
+    this.isGameOver = true;
+    let winnerString = "";
+
+
     const victoryPoints = new Array(this.players.length);
     let highestPoints = 0;
     for (let i = 0; i < this.players.length; i++) {
@@ -142,24 +208,24 @@ export class GameComponent implements OnInit, AfterViewInit {
       if (victoryPoints[i] === highestPoints) {
         if (firstVictor) {
           firstVictor = false;
-          this.winnerString = this.players[i].player;
+          winnerString = this.players[i].player;
         } else {
-          this.winnerString = this.winnerString + ', ' + this.players[i].player;
+          winnerString = winnerString + ', ' + this.players[i].player;
         }
       }
     }
+
     // TODO: pass winnerString to the dialog somehow. This should happen here.
     this.winnerDialogRef = this.dialog.open(WinnerDialogComponent, {
+      data: {
+        winnerName: "Hallo"
+      },
       width: '400px',
       height: '400px'
     });
+
+
   }
 
-  /*
-  * To start the winnerdialog:
-  * this.winnerDialogRef = this.dialog.open(WinnerDialogComponent{
-        width: '400px',
-        height: '400px'
-      });
-  */
+
 }
