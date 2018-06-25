94666bfb0fcdc88711a7e18d8af6826f4e15019a
pa6ne6 <pa6ne6@PC474W>
2018-06-25 09:09:55 +0200

    Rearranged code

--- PMGame/src/app/winner-dialog/winner-dialog.component.ts
+++ PMGame/src/app/winner-dialog/winner-dialog.component.ts
@@ -1,14 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef } from '@angular/material';
+import { MatDialog } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-winner-dialog',
   templateUrl: './winner-dialog.component.html',
   styleUrls: ['./winner-dialog.component.scss']
 })
+
+
 export class WinnerDialogComponent implements OnInit {
-  constructor() {}
+  
+  
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public winnerName: string,
+    public dialogRef: MatDialogRef<WinnerDialogComponent>
+  ) { }
+  
+  ngOnInit() { }
 
-  ngOnInit() {}
 
-  winnerString = 'DummyString';
+  onClickOk() {
+    this.dialogRef.close();
+  }
+
 }
